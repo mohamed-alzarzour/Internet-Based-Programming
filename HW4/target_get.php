290005393eec6eaddebb9b7f873e47c8089cc306
mohamed-alzarzour <109822177+mohamed-alzarzour@users.noreply.github.com>
2023-05-19 20:27:27 +0300

    Add files via upload

---
+++ HW4/target_get.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>GET Form Submission Result</title>
+</head>
+<body>
+  <h1>GET Form Submission Result</h1>
+  <p>Name: <?php echo $_GET['name']; ?></p>
+  <p>Email: <?php echo $_GET['email']; ?></p>
+</body>
+</html>
---
+++ HW4/target_post.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>POST Form Submission Result</title>
+</head>
+<body>
+  <h1>POST Form Submission Result</h1>
+  <p>Name: <?php echo $_POST['name']; ?></p>
+  <p>Email: <?php echo $_POST['email']; ?></p>
+</body>
+</html>
